cc9d554ca9d9de49a04ae9484d6177d4bec1cf84
Nevss <mr.nevss@gmail.com>
2021-10-02 15:00:28 +0700

    Deleted format-string

--- lecture03/2.py
+++ lecture03/2.py
@@ -9,7 +9,7 @@ while True:
         A = int(input("Введите число A: "))
         B = int(input("Введите число B: "))
         if gcd(A, B):
-            print("Наибольший общий делитель чисел {A} и {B}:".format(A, B), gcd(A, B))
+            print("Наибольший общий делитель чисел A и B:", gcd(A, B))
         else:
             print("Наибольший общий делитель не найден :(")
         exit(0)
