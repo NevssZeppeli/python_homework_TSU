df6a85cd9c8800b2576e56bc0dc3837e82ec6d35
Nevss <mr.nevss@gmail.com>
2021-10-04 20:47:26 +0700

    Changed algorythm for is_prime method

--- lecture03/1.py
+++ lecture03/1.py
@@ -1,17 +1,28 @@
 # для ЕГЭ я использовал этот алгоритм, для решета Эратосфена напишу потом
-def is_prime(x) -> bool:
-    sq = int(x**0.5)
-    for i in range(2, sq+1):
-        if x % i == 0:
-            return False
-    return True
+#def is_prime(x) -> bool:
+#    sq = int(x**0.5)
+#    for i in range(2, sq+1):
+#        if x % i == 0:
+#            return False
+#    return True
+
+
+def is_prime(N):
+    # это я делаю для того, чтобы само число N тоже проверялось!
+    N += 1
+    A = [True] * N
+    A[0] = A[1] = False
+    for k in range(2, int(N**0.5)):
+        if A[k]:
+            for m in range(2*k, N, k):
+                A[m] = False
+    return [k for k in range(N) if A[k]]
 
 
 while True:
     try:
         N = int(input("Введите целое число N: "))
-        primes = [x for x in range(2, N) if is_prime(x)]
-        print("Простые числа от 2 до N:", *primes)
+        print("Простые числа от 2 до N:", *is_prime(N))
         exit(0)
     except (TypeError, ValueError) as e:
-        print("Упс, что-то пошло не так, попробуйте снова ввести целое число!")
+        print("Упс, что-то пошло не так, попробуйте снова ввести целое число!", e)
